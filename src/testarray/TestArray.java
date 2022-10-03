fe8e7ad18288d9ed1ec800a04ff499f5441b0a5d
Ignacio Correa <nachitoficopal@gmail.com>
2022-10-03 12:16:11 +0200

    Array
    Arrya

---
+++ src/testarray/TestArray.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testarray;
+
+import java.lang.reflect.Array;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class TestArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Array n[]= new Array [5];
+        for (int i = 0; i < n.length; i++) { 
+            System.out.println(" "+(i+1)+"\t");
+        }
+        
+    }
+    
+}
